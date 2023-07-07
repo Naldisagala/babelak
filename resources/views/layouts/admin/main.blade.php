d439264a8176d517bfc3654fc4f0965ea4d54e52
budiono1 <budiono@limecommerce.com>
2023-07-07 08:54:13 +0700

    full width export pdf

--- resources/views/layouts/admin/main.blade.php
+++ resources/views/layouts/admin/main.blade.php
@@ -133,6 +133,10 @@
                     extend: 'pdfHtml5',
                     exportOptions: {
                         columns: column_show
+                    },
+                    customize: function(doc) {
+                        doc.content[1].table.widths =
+                            Array(doc.content[1].table.body[0].length + 1).join('*').split('');
                     }
                 },
             ]
@@ -150,8 +154,9 @@
                 },
                 {
                     extend: 'pdfHtml5',
-                    exportOptions: {
-                        columns: column_show
+                    customize: function(doc) {
+                        doc.content[1].table.widths =
+                            Array(doc.content[1].table.body[0].length + 1).join('*').split('');
                     }
                 },
             ]
@@ -171,6 +176,10 @@
                     extend: 'pdfHtml5',
                     exportOptions: {
                         columns: column_show
+                    },
+                    customize: function(doc) {
+                        doc.content[1].table.widths =
+                            Array(doc.content[1].table.body[0].length + 1).join('*').split('');
                     }
                 },
             ]
